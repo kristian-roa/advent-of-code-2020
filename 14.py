62f87bdf6942b8eed0c0b637708b76a3fc0b4802
Kristian <krisrgra@uio.no>
2020-12-13 19:10:14 +0100

    initialized

---
+++ 14.py
@@ -0,0 +1,15 @@
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+def main():
+    with open('input_14.txt') as doc: inp = doc.read().splitlines()
+    print(part_1())
+    print(part_2())
+
+
+main()
